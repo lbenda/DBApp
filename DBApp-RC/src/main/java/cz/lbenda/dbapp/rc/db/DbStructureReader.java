b27291edf704966f0398770a6b1c42dc4a6ef622
Lukas Benda <lbenda.work@gmail.com>
2014-09-14 21:43:05 +0200

    Solve trivial compilation problem

--- DBApp-RC/src/main/java/cz/lbenda/dbapp/rc/db/DbStructureReader.java
+++ DBApp-RC/src/main/java/cz/lbenda/dbapp/rc/db/DbStructureReader.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List
+import java.util.List;
 import java.util.Map;
 
 /** Class for reading data structure from JDBC
